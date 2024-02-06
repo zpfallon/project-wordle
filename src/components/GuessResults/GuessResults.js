2d79ee2d2033399317bde70092efb0425cddf9a2
Zachary Fallon <zfallon@marathonus.com>
2024-02-06 10:23:28 -0500

    Exercise 3 Solution

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,10 +1,14 @@
 import React from "react";
 
+import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import Guess from "../Guess/Guess";
+
 function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess, index) => (
-        <p key={index} className="guess">{guess}</p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={guesses[num]} />
       ))}
     </div>
   );
